03e6d5f7bda0d7c7415b437515ff46c434e20321
ola <ola@cs.duke.edu>
2017-01-12 11:14:32 -0500

    first commit

---
+++ app/src/main/java/compsci290/edu/duke/geoquiz/QuizActivity.java
@@ -0,0 +1,49 @@
+package compsci290.edu.duke.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    private class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) this.findViewById(R.id.true_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this,
+                               R.string.correct_toast,
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) this.findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                               R.string.incorrect_toast,
+                               Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
